a52f232d6d27e15a283f58e236c1499bd39649b9
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-07-12 22:08:14 +0530

    implement dense layer

---
+++ nn/layers/dense.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from ..initializers.random_normal import RandomNormalInitializer
+from .base import Layer
+
+
+class Dense(Layer):
+    def __init__(self, n_inputs: int, n_neurons: int, initializer=None):
+        if initializer is None:
+            initializer = RandomNormalInitializer()
+        self.weights = initializer.initialize(shape=(n_inputs, n_neurons))
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        output = inputs @ self.weights + self.biases
+        return output
+
+    def backward(self, grad_output):
+        self.dweights = self.inputs.T @ grad_output
+        self.dbiases = np.sum(grad_output, axis=0, keepdims=True)
+        self.dinputs = grad_output @ self.weights.T
+        return self.dinputs
