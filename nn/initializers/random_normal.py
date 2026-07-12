40f2c6c44c919b1c4c0d2dbaff5275c6184c4c4c
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-07-12 19:05:35 +0530

    create normal initializer

---
+++ nn/initializers/random_normal.py
@@ -0,0 +1,12 @@
+import numpy as np
+from base import Initializer
+
+
+class RandomNormalInitializer(Initializer):
+    def __init__(self, mean: float = 0, std: float = 0.01, seed: int = None):
+        self.mean = mean
+        self.std = std
+        self.rng = np.random.default_rng(seed)
+
+    def initialize(self, shape: tuple[int, ...]):
+        return self.rng.normal(self.mean, self.std, size=shape)
