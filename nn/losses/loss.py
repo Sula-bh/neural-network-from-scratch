804b0f4e0483759571f14181266c2ac6048291aa
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-07-13 11:36:15 +0530

    change parameter name

--- nn/losses/loss.py
+++ nn/losses/loss.py
@@ -5,14 +5,14 @@ import numpy as np
 
 class Loss(ABC):
     @abstractmethod
-    def forward(self, y_pred, y_true) -> np.ndarray:
+    def forward(self, predictions, targets) -> np.ndarray:
         pass
 
     @abstractmethod
     def backward(self) -> None:
         pass
 
-    def calculate(self, y_pred, y_true) -> float:
-        sample_losses = self.forward(y_pred, y_true)
+    def calculate(self, predictions, targets) -> float:
+        sample_losses = self.forward(predictions, targets)
         data_loss = np.mean(sample_losses)
         return data_loss
